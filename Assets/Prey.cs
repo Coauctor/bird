506113ad8793501f307b9f471e82e81510f55f30
bohdan.sereda <bohdan.sereda@creative-mobile.com>
2018-06-23 15:04:25 +0300

    flight iteration

---
+++ Assets/Prey.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prey : MonoBehaviour
+{
+
+	public Transform Mouse;
+	public Transform Hunter;
+	public Transform Circle;
+	public Collider Eagle;
+	
+
+	private float _hunterPos;
+	private float _mousePos;
+
+	private void Start()
+	{
+		_hunterPos = Hunter.transform.position.y;
+		_mousePos = Mouse.transform.position.y;
+	}
+
+	void Update () 
+	
+	{
+		if (_hunterPos > _mousePos)
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
+
+	private void OnCollisionEnter(Collision Hunter)
+	{
+		Destroy(Mouse);
+	}
+}
